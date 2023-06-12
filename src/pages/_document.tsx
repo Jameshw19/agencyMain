d59f5785a817e3a08058dc6f759329a6dece79c3
Jameshw19 <108671331+Jameshw19@users.noreply.github.com>
2023-06-12 21:11:28 +0100

    changed title to we-mu, and added we-mu logo to head in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>We-Mu</title>
+        <link rel="icon" href="assets/logo.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
